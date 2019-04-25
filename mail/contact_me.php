c5ff8df55af662285175d58fe85e97565ff1f363
Wenderson Jr <wendersonej.74@gmail.com>
2019-04-24 21:02:39 -0300

    Adição das imagens finais, ajuste no css e remoção da página about

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,26 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
-?>
+	// Check for empty fields
+	if(
+		empty($_POST['name'])     ||
+		empty($_POST['email'])    ||
+		empty($_POST['phone'])    ||
+		empty($_POST['message'])  ||
+		!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)
+	) {
+		echo "No arguments Provided!";
+		return false;
+	}
+
+	$name = strip_tags(htmlspecialchars($_POST['name']));
+	$email_address = strip_tags(htmlspecialchars($_POST['email']));
+	$phone = strip_tags(htmlspecialchars($_POST['phone']));
+	$message = strip_tags(htmlspecialchars($_POST['message']));
+
+	$to = 'wendersonej.74@gmail.com';
+	$email_subject = "Enviado por:  $name";
+	$email_body = "Você recebeu uma nova mensagem do seu site.\n\n"."Veja os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+	$headers = "De: wendersonej.74@gmail.com\n";
+	$headers .= "Respondendo a: $email_address";   
+	mail($to,$email_subject,$email_body,$headers);
+	return true;         
+     
